82aaeab5a7cf8d5e21a1a01c7c27a320dc96a206
arstrof <117746931+arstrof@users.noreply.github.com>
2024-05-26 19:25:02 +0300

    added PlacePiece and globals

--- main.py
+++ main.py
@@ -1,2 +1,59 @@
+<<<<<<< Updated upstream
 print("This will be Quarto")
 print("Just testing Github")
+=======
+print("This will be Quarto, Arsenii's branch")
+
+
+
+from PlacePiece import PlacePiece
+
+PlacePiece("LHSN")
+
+
+#what we need
+"""
+main variables
+1. 1D list for the left objects
+2. 2D list for the board
+3. Current player 
+
+functions/modules:
+1. Is there 4 in a row. 
+    inputs list for the board
+    returns boolean if there is 4 in a row, otherwise false
+2. inputing the next object to place(with all the checking) or saying quarto:
+    inputs all list for the left objects
+    returns next object and list of the left objects
+3. placing the object on the board 
+    inputs object, board list
+    prompits and inputs from the player the coordinates, checks them
+    returns new board 
+
+Interface: 
+board with coordinates
+
+
+LHSN
+LHCN
+LTSN
+LTCN
+LHSY
+LHCY
+LTSY
+LTCY
+DHSN
+DHCN
+DTSN
+DTCN
+DHSY
+DHCY
+DTSY
+DTCY
+
+"""
+
+
+
+
+>>>>>>> Stashed changes
