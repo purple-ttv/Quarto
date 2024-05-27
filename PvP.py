ea898d1c787603c3781c750d936ebcf4f9144180
arstrof <117746931+arstrof@users.noreply.github.com>
2024-05-27 18:56:08 +0300

    added PvP and PvE modes in main.py

---
+++ PvP.py
@@ -0,0 +1,57 @@
+# This file was written by Vakaris
+
+# --- Importing all modules ---
+import time
+import globals
+from Greeting import OutputGreeting
+from ShowBoard import ShowBoard
+from LeftPieces import UpdatePieceList
+from TakeFirstInput import TakeFirstInput
+from MovePrompt import WhoseMove
+from PlacePiece import PlacePiece
+from PieceInput import TakePieceInput
+from CheckBoard import CheckBoard
+from Ending import Ending
+from ResolveStatus import ResolveStatus
+
+
+# --- MAIN ---
+
+print("You chose to play PvP(Person versus Person)")
+
+ShowBoard()                     # The board is shown for the first player
+
+Counter = 0                     # Counter to track the move number
+IsOver = False                  # Flag used to break out of the main loop
+
+WhoseMove(1)                    # Prompt to show it's Player 1's move
+Piece = TakeFirstInput()        # Player one chooses his first piece
+                                # Different function, as player can choose a piece himself
+
+while (globals.Status == 0):    # Loop will stop when the global Status variable is changed (indicating the ste of the game)
+    Counter += 1
+    if (Counter != 1):          # Skipping the first counter to avoid prompting twice
+        WhoseMove(Counter)      # Prompt that shows whose move it is
+    PlacePiece(Piece)           # Player places the given piece
+    ShowBoard()                 # Updated board is shown
+
+    if (globals.Status == 0) and (Counter == 16):
+        globals.Status = 3      # To indicate a draw ("and" needed to avoid a problem when a player wins on last move)
+        break                   # Loop needs to end before taking another piece input, as no more pieces left in a draw
+    
+    Piece = TakePieceInput()    # Player either chooses a piece for opponent or says "Quarto" if he believes he can win
+    while (Piece == "Quarto"):
+        if (CheckBoard()):      # If a Quarto is present on the board, the current player wins, else play continues
+            ResolveStatus(Counter)
+            IsOver = True
+            break               # Needed as value of Piece doesn't actually change, i.e. loop wouldn't be exited
+        else:                   # I.e. player called Quarto incorrectly, play proceeds after player enters a valid piece
+            print("There was no Quarto!")
+            Piece = TakePieceInput()
+
+    if IsOver == True:
+        break                   # Loop is exited fully in case game finished
+
+    UpdatePieceList(Piece)      # Only executed when Piece is for sure on the LeftPieces list (previous loop ensures this)
+
+Ending(globals.Status)          # Finally, when game is over, a final message is output based on the result
--- main.py
+++ main.py
@@ -1,55 +1,24 @@
-# This file was written by Vakaris
+# This file was written by Arsenii
 
 # --- Importing all modules ---
-import time
-import globals
 from Greeting import OutputGreeting
-from ShowBoard import ShowBoard
-from LeftPieces import UpdatePieceList
-from TakeFirstInput import TakeFirstInput
-from MovePrompt import WhoseMove
-from PlacePiece import PlacePiece
-from PieceInput import TakePieceInput
-from CheckBoard import CheckBoard
-from Ending import Ending
-from ResolveStatus import ResolveStatus
 
 
 # --- MAIN ---
 OutputGreeting()                # Showing a greeting message before the game starts
-ShowBoard()                     # The board is shown for the first player
 
-Counter = 0                     # Counter to track the move number
-IsOver = False                  # Flag used to break out of the main loop
+GameMode = input("Would you like to play PvP or PvE? ")
 
-WhoseMove(1)                    # Prompt to show it's Player 1's move
-Piece = TakeFirstInput()        # Player one chooses his first piece
-                                # Different function, as player can choose a piece himself
+while GameMode != "END":
+        
 
-while (globals.Status == 0):    # Loop will stop when the global Status variable is changed (indicating the ste of the game)
-    Counter += 1
-    if (Counter != 1):          # Skipping the first counter to avoid prompting twice
-        WhoseMove(Counter)      # Prompt that shows whose move it is
-    PlacePiece(Piece)           # Player places the given piece
-    ShowBoard()                 # Updated board is shown
+    if GameMode == "PvP":
+        with open("PvP") as file:
+            exec(file.read())
+    elif GameMode == "PvE":
+        with open("PvE") as file:
+            exec(file.read())
 
-    if (globals.Status == 0) and (Counter == 16):
-        globals.Status = 3      # To indicate a draw ("and" needed to avoid a problem when a player wins on last move)
-        break                   # Loop needs to end before taking another piece input, as no more pieces left in a draw
-    
-    Piece = TakePieceInput()    # Player either chooses a piece for opponent or says "Quarto" if he believes he can win
-    while (Piece == "Quarto"):
-        if (CheckBoard()):      # If a Quarto is present on the board, the current player wins, else play continues
-            ResolveStatus(Counter)
-            IsOver = True
-            break               # Needed as value of Piece doesn't actually change, i.e. loop wouldn't be exited
-        else:                   # I.e. player called Quarto incorrectly, play proceeds after player enters a valid piece
-            print("There was no Quarto!")
-            Piece = TakePieceInput()
+    GameMode = input("Would you like to play PvP or PvE? (you may write END to exit the game) ")
 
-    if IsOver == True:
-        break                   # Loop is exited fully in case game finished
-
-    UpdatePieceList(Piece)      # Only executed when Piece is for sure on the LeftPieces list (previous loop ensures this)
-
-Ending(globals.Status)          # Finally, when game is over, a final message is output based on the result
+print("See you later!")
