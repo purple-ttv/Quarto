58cdd96b2c8daadcf63ea3807ced99bed7c6d438
purple-ttv <vakaris.gavenas@gmail.com>
2024-05-27 13:25:47 +0300

    Update globals

--- globals.py
+++ globals.py
@@ -5,6 +5,7 @@ Board = [["----", "----", "----", "----"],["----", "----", "----", "----"],["---
 
 # 1D arrays for storing available pieces and positions
 LeftPieces = ["LHSN","LHCN","LTSN","LTCN","LHSY","LHCY","LTSY","LTCY","DHSN","DHCN","DTSN","DTCN","DHSY","DHCY","DTSY","DTCY"]
+AvailableCoordinates = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
 
 # Status used as a flag to end the game, as well as track who won:
 # 0 - on-going, 1 - Player1 won, 2 - Player2 won, 3 - it's a draw
